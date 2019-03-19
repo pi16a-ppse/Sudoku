5284524ee34d043e2d7c7569cc143dc78f8a614e
GruzanSergey <656eee@mail.ru>
2019-03-19 18:58:19 +0300

    Изменена сложность игры

--- Sudoku/sudoku.js
+++ Sudoku/sudoku.js
@@ -4,7 +4,7 @@ function generate() {
 	for (var j=0;j<9;j++) arr.push(arr.splice((Math.random() * arr.length), 1));
 	for (var i=1;i<82;i++) { 
 		if (i%9==1) document.write('<tr>');
-		document.write( (Math.random()*10>5) ? '<td>'+arr[field.substr(i,1)-1] + '</td>' : '<td class="l"><input type="text" size="1" maxlength="1" onchange="test()" onkeydown = "javascript: return ((event.keyCode>47)&&(event.keyCode<58))"></td>' );
+		document.write( (Math.random()*10>6) ? '<td>'+arr[field.substr(i,1)-1] + '</td>' : '<td class="l"><input type="text" size="1" maxlength="1" onchange="test()" onkeydown = "javascript: return ((event.keyCode>47)&&(event.keyCode<58))"></td>' );
 		if (i%9==0) document.write('</tr>');
 	}
 }
